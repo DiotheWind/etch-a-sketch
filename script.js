6bd8747483dee24f1109d29c6cdb58f26488d722
Deo Seth <diothewind@gmail.com>
2023-07-16 11:10:54 +0800

    Add comments

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const gridContainer = document.querySelector("#grid-container");
 const changeGridSize = document.querySelector("#change-grid-size");
 const clearGridBtn = document.querySelector("#clear-grid");
 
-let gridSizeValue = 16;
-let toDraw;
+let gridSizeValue = 16; 
+let toDraw;  // Flag variable for the hold click
 
 generateGrid(gridSizeValue);
-gridContainer.onmousedown = () => toDraw = true;
-gridContainer.onmouseup = () => toDraw = false;
+gridContainer.onmousedown = () => toDraw = true; // The user can shade the cell if
+gridContainer.onmouseup = () => toDraw = false;  // the mouse was on hold click inside that specific cell
 
 changeGridSize.addEventListener("click", () => {
     const gridSizeValuePrompt = prompt("Enter grid size between 10 and 64");
@@ -16,7 +16,7 @@ changeGridSize.addEventListener("click", () => {
         clearGrid();
         generateGrid(gridSizeValue);
     } else {
-        alert("Invalid grid size.")
+        alert("Invalid grid size.");
     }
 });
 
@@ -26,16 +26,16 @@ clearGridBtn.addEventListener("click", () => {
 });
 
 function generateGrid(gridSize) {
-    for (let i = 1; i <= gridSize; i++) {
+    for (let i = 1; i <= gridSize; i++) {  // Generate rows of container for the cell
         const row = document.createElement("div");
         row.className = "row";
-        for (let j = 1; j <= gridSize; j++) {
+        for (let j = 1; j <= gridSize; j++) {  // Generates cells to be stored inside the row container
             const cell = document.createElement("div");
             cell.className = "cell";
             cell.setAttribute("id", "cell");
             cell.addEventListener("mouseenter", () => {
                 if (toDraw) {
-                    cell.classList.add("cell-change-color");
+                    cell.classList.add("cell-change-color");  // The cell changes color if the ToDraw flag variable is set to True
                 }
             });
             row.appendChild(cell);
