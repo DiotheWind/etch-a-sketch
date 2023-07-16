38ad51f64743b3d6f4cec999221109763aa63167
Deo Seth <diothewind@gmail.com>
2023-07-16 09:39:27 +0800

    Add functionality to change-grid-size button

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const gridContainer = document.querySelector("#grid-container");
+const changeGridSize = document.querySelector("#change-grid-size");
 const clearGridBtn = document.querySelector("#clear-grid");
 
+let gridSize = 16;
+
 generateGrid();
+
+changeGridSize.addEventListener("click", () => {
+    gridContainer.innerHTML = "";
+    const gridSizeValue = prompt("Enter grid size between 10 and 64");
+    if (gridSizeValue >= 10 && gridSizeValue <= 64) {
+        gridSize = gridSizeValue;
+    } else {
+        alert("Invalid grid size.")
+    }
+    generateGrid();
+});
 
 clearGridBtn.addEventListener("click", () => {
     gridContainer.innerHTML = "";
@@ -9,10 +23,10 @@ clearGridBtn.addEventListener("click", () => {
 });
 
 function generateGrid(){
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= gridSize; i++) {
         const row = document.createElement("div");
         row.className = "row";
-        for (let j = 1; j <= 16; j++) {
+        for (let j = 1; j <= gridSize; j++) {
             const cell = document.createElement("div");
             cell.className = "cell";
             cell.setAttribute("id", "cell")
