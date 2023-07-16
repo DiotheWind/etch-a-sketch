0abf6674de37fcba411089346467d441a95fec03
Deo Seth <diothewind@gmail.com>
2023-07-16 09:26:19 +0800

    Add functionality to clear button where it clears the grid

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const gridContainer = document.querySelector("#grid-container");
+const clearGridBtn = document.querySelector("#clear-grid");
 
-for (let i = 1; i <= 16; i++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    for (let j = 1; j <= 16; j++) {
-        const cell = document.createElement("div");
-        cell.className = "cell";
-        cell.setAttribute("id", "cell")
-        cell.addEventListener("mouseover", () => cell.classList.add("cell-change-color"))
-        row.appendChild(cell);
+generateGrid();
+
+clearGridBtn.addEventListener("click", () => {
+    gridContainer.innerHTML = "";
+    generateGrid();
+});
+
+function generateGrid(){
+    for (let i = 1; i <= 16; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        for (let j = 1; j <= 16; j++) {
+            const cell = document.createElement("div");
+            cell.className = "cell";
+            cell.setAttribute("id", "cell")
+            cell.addEventListener("mouseover", () => cell.classList.add("cell-change-color"))
+            row.appendChild(cell);
+        }
+        gridContainer.appendChild(row);
     }
-    gridContainer.appendChild(row);
 }
+
