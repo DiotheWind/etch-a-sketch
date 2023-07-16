a3b3d04dd2fa533128da1cebd5d3da048e4042ad
Deo Seth <diothewind@gmail.com>
2023-07-16 09:15:44 +0800

    Add functionality where cells change color when a mouse hovers over it

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ for (let i = 1; i <= 16; i++) {
     for (let j = 1; j <= 16; j++) {
         const cell = document.createElement("div");
         cell.className = "cell";
+        cell.setAttribute("id", "cell")
+        cell.addEventListener("mouseover", () => cell.classList.add("cell-change-color"))
         row.appendChild(cell);
     }
     gridContainer.appendChild(row);
